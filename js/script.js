b07f0044021a5e11069d8d101d946ef5cea2bc75
nataliarestrada <nati.tachito@gmail.com>
2022-03-25 04:17:25 -0300

    Header, Home y About

---
+++ js/script.js
@@ -0,0 +1,55 @@
+var type = new Typed('.typing-text',{
+    strings : [ 'web designer', 'front end developer', 'graphic designer', 'photographer' ],
+    typeSpeed: 120,
+    loop:true
+});
+
+let themeColor = document.querySelectorAll('.theme-toggler span');
+themeColor.forEach(color => color.addEventListener('click', () =>{
+    let background = color.style.background;
+    document.querySelector('body').style.background = background;
+}));
+
+
+
+
+
+
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+      $(this).toggleClass('fa-times');
+      $('header').toggleClass('toggle');
+    });
+  
+    $(window).on('scroll load',function(){
+  
+      $('#menu').removeClass('fa-times');
+      $('header').removeClass('toggle');
+  
+      if($(window).scrollTop() > 0){
+        $('.top').show();
+      }else{
+        $('.top').hide();
+      }
+  
+    });
+  
+    // smooth scrolling 
+  
+    $('a[href*="#"]').on('click',function(e){
+  
+      e.preventDefault();
+  
+      $('html, body').animate({
+  
+        scrollTop : $($(this).attr('href')).offset().top,
+  
+      },
+        500, 
+        'linear'
+      );
+  
+    });
+  
+});
